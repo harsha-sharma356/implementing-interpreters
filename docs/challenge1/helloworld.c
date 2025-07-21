eaaa1f85155c7b1db0a1a9d0e49cabe7559ce065
Harsha Sharma <hsha0051@student.monash.edu>
2025-07-21 11:48:40 +1000

    added code from craftinginterpreters.com that allows for running files via command line

---
+++ docs/challenge1/helloworld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct node{
+    struct node* previous;
+    char* value;
+    struct node* next;
+};
+
+int main(void){
+    char* string = malloc(50);
+    string = "hello world";
+    struct node* MasterNode = malloc(sizeof(struct node));
+    MasterNode
+    printf("hello world");
+    return 8008135;
+}
